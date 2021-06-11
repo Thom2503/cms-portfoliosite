4d5423922c312b302805c3bdd331157aa63ee26d
Thom Veldhuis <thomveldhuis03@gmail.com>
2021-06-11 19:55:04 +0200

    Added sql for the database

---
+++ cms.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 11, 2021 at 07:54 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.3.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media`
+--
+
+CREATE TABLE `media` (
+  `ID` int(11) NOT NULL,
+  `Type` varchar(32) NOT NULL,
+  `Name` varchar(256) NOT NULL,
+  `Project_ID` char(36) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `opleiding`
+--
+
+CREATE TABLE `opleiding` (
+  `OpleidingID` int(11) NOT NULL,
+  `Naam` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `opleiding`
+--
+
+INSERT INTO `opleiding` (`OpleidingID`, `Naam`) VALUES
+(1, 'Mediavormgeven'),
+(2, 'Creative Productie'),
+(3, 'Mediamanagement'),
+(4, 'Redactiemedewerker'),
+(5, 'Mediatechnologie'),
+(6, 'Audiovisuele Media'),
+(7, 'Podium- en evenemententechniek');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `ID` char(36) NOT NULL,
+  `Titel` varchar(128) NOT NULL,
+  `Omschrijving` text NOT NULL,
+  `Datum` date NOT NULL,
+  `User_ID` char(36) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` char(36) NOT NULL,
+  `Type` int(2) NOT NULL,
+  `Naam` varchar(128) NOT NULL,
+  `Achternaam` varchar(128) NOT NULL,
+  `Email` varchar(128) NOT NULL,
+  `Username` varchar(128) NOT NULL,
+  `Password` varchar(64) NOT NULL,
+  `Opleiding_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE `user_info` (
+  `ID` int(11) NOT NULL,
+  `User_ID` char(36) NOT NULL,
+  `About` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `media`
+--
+ALTER TABLE `media`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `Project_ID` (`Project_ID`);
+
+--
+-- Indexes for table `opleiding`
+--
+ALTER TABLE `opleiding`
+  ADD PRIMARY KEY (`OpleidingID`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `User_ID` (`User_ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`),
+  ADD KEY `Opleiding_ID` (`Opleiding_ID`);
+
+--
+-- Indexes for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `User_ID` (`User_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `media`
+--
+ALTER TABLE `media`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `opleiding`
+--
+ALTER TABLE `opleiding`
+  MODIFY `OpleidingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user_info`
+--
+ALTER TABLE `user_info`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `media`
+--
+ALTER TABLE `media`
+  ADD CONSTRAINT `media_ibfk_1` FOREIGN KEY (`Project_ID`) REFERENCES `project` (`ID`);
+
+--
+-- Constraints for table `project`
+--
+ALTER TABLE `project`
+  ADD CONSTRAINT `project_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `users` (`UserID`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`Opleiding_ID`) REFERENCES `opleiding` (`OpleidingID`);
+
+--
+-- Constraints for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD CONSTRAINT `user_info_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `users` (`UserID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
