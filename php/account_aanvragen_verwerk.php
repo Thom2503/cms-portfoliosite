30d679f480971313e64d7e6d7b0a4ff5f32e0f11
Thom Veldhuis <thomveldhuis03@gmail.com>
2021-06-16 16:15:42 +0200

    you can now request an account as a company

--- php/account_aanvragen_verwerk.php
+++ php/account_aanvragen_verwerk.php
@@ -8,6 +8,7 @@
 	$date = date('m/d/Y h:i:s a', time());
 
   include "../includes/html.php";
+  include "../includes/email.php";
   include "config.php";
 
   //function om errors makkelijker te maken. en maakt het wat mooier uitzien
@@ -15,6 +16,29 @@
   {
     echo $message."<br>";
     echo "<button onclick='history.back(); return false;'>Ga terug</button>";
+  }
+
+  function send_mail($bedrijf, $voornaam, $achternaam, $email)
+  {
+    $to_email = "thomveldhuis03@gmail.com";
+    $subject = "Aanvraag account voor Portfoliosite - $bedrijf";
+    $body = "
+      Er is een aanvraag gedaan om een account aan te maken:
+      Naam bedrijf: $bedrijf
+      Naam aanvrager: $voornaam $achternaam
+      Email: $email
+
+      Wilt u dit bedrijf toegang geven om een account aan te maken?
+      Klik de link:
+
+    ";
+    $headers[] = "From: Portfoliosite account vrager";
+
+    if (mail($to_email, $subject, $body, implode('\r\n', $headers))) {
+        echo "Email successvol verzonden...<br> <button onclick='history.back(); return false;'>Ga terug</button>";
+    } else {
+        echo "Email verzenden fout gegaan... <br> <button onclick='history.back(); return false;'>Ga terug</button>";
+    }
   }
 
   display_header($name = NULL, "Account aanvragen verwerken...", $log = false, $inFolder = true); //functie voor de rand html, is simpel for mooiheid
@@ -26,7 +50,23 @@
         {
           if (isset($_POST['submit']))
           {
-
+            $bedrijf = htmlspecialchars($_POST['bedrijfsnaam'], ENT_QUOTES);
+            $voornaam = htmlspecialchars($_POST['voornaam'], ENT_QUOTES);
+            $achternaam = htmlspecialchars($_POST['achternaam'], ENT_QUOTES);
+            $email = $_POST['email'];
+            if(!empty($voornaam) || !empty($achternaam) || !empty($email) || !empty($bedrijf))
+            {
+              if (email_validate($email))
+              {
+                send_mail($bedrijf, $voornaam, $achternaam, $email);
+              } else
+              {
+                error("Emailadres klopt niet!");
+              }
+            } else
+            {
+              error("Sommige velden zijn leeg gelaten!");
+            }
           }
         } else
         {
