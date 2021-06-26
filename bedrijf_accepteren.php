810c9a9749f01ba51b3bdbaaa21b8c4335329433
Thom2503 <thomveldhuis03@gmail.com>
2021-06-26 22:43:35 +0200

    added bedrijf accepteren (needs to be done) and added some links in index

--- bedrijf_accepteren.php
+++ bedrijf_accepteren.php
@@ -3,18 +3,17 @@
   session_start();
 
   include "includes/html.php";
-  include "includes/opleidingen.php";
 
-  display_header(NULL, "Inloggen", false);
+  display_header(NULL, "Bedrijf accepteren", false);
 
   $token = bin2hex(openssl_random_pseudo_bytes(32));
 	$_SESSION['token'] = $token;
 
   ?>
     <main>
-      <form class="form" action="php/inloggen_verwerk.php" method="post">
+      <form class="form" action="" method="post">
         <div class="centerAlign">
-          <h2>> Inloggen</h2>
+          <h2>> Bedrijf accepteren</h2>
           <input type="hidden" name="csrf_token" value="<?php echo $token; ?>"/>
           <h3>> Gebruikersnaam</h3>
           <input style="width: 300px; height: 50px; " type="text" name="gebruikersnaam" placeholder="Gebruikersnaam" required>
@@ -22,8 +21,7 @@
           <input style="width: 300px; height: 50px; " type="password" name="wachtwoord" placeholder="Wachtwoord" required>
         </div>
         <br><br>
-        <input class="submit" type="Submit" name="submit" value="Inloggen"><br>
-        <p class="centerAlign">Heeft u nog geen acoount klik dan hier: <br> <a href="account_maken.php">Voor studenten</a> en <a href="account_aanvragen.php">voor bedrijven</a> </p>
+        <input class="submit" type="Submit" name="submit" value="Inloggen">
       </form>
     </main>
   <?php
